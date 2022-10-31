2d1fe768c16e6267183436e3af3f156d8f1730bd
belyakova1407 <belyakova1407@gmail.com>
2022-10-31 13:23:45 +0200

    css style added

--- src/index.js
+++ src/index.js
@@ -83,3 +83,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Kyiv");
