3e129274ec2720d58cc95020ad20db4b65cf624b
belyakova1407 <belyakova1407@gmail.com>
2022-11-03 13:45:10 +0200

    adding Api

--- src/index.js
+++ src/index.js
@@ -22,6 +22,40 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+       <div class="col-2">
+         <div class="weather-forecast-date">${day}</div>
+         <img
+           src="http://openweathermap.org/img/wn/50d@2x.png"
+           alt=""
+           width="42"
+         />
+         <div class="weather-forecast-temperatures">
+           <span class="weather-forecast-temperature-max"> 18° </span>
+           <span class="weather-forecast-temperature-min"> 12° </span>
+         </div>
+       </div>
+       `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "5f928f6fdcef3a884fbde90b505b3440";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
